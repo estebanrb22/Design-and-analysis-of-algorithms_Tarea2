fa7680a1816bf1f88ad26d4dde2f5e4ffbe7f7fe
ChileanToaster <ricfernandezreyes@gmail.com>
2024-06-21 00:38:03 -0400

    Minor refactor to heap structure, and implementation of min, extractMin and decreaseKey.

--- dijkstra_heap.cpp
+++ dijkstra_heap.cpp
@@ -16,8 +16,15 @@ using namespace std;
 // “Introduction to Algorithms Third Edition”
 // de Cormen et. al. 
 
-typedef vector<infoNode> Heap;
-typedef vector<infoNode*> NodePair;
+
+class HeapNode {
+    public:
+        infoNode value;
+        int pos; // Posición dentro de la cola
+};
+
+typedef vector<HeapNode> Heap;
+typedef vector<HeapNode*> NodePair;
 
 class MinHeap {
     public:
@@ -31,6 +38,10 @@ class MinHeap {
         nodepair.reserve(heapSize);
     }
 };
+
+int parent(int i){
+    return (i-1) / 2;
+}
 
 // Retornar hijo izquierdo del nodo i
 int left(int i){
@@ -47,22 +58,23 @@ void minHeapify(MinHeap& A, int i){ // O(log n)
     int l = left(i);
     int r = right(i);
     int smallest;
-    if(l < A.size && A.priqueue[l].dist < A.priqueue[i].dist){
+    if(l < A.size && A.priqueue[l].value.dist < A.priqueue[i].value.dist){
         smallest = l;
     } else {
         smallest = i;
     }
-    if(r < A.size && A.priqueue[r].dist < A.priqueue[smallest].dist){
+    if(r < A.size && A.priqueue[r].value.dist < A.priqueue[smallest].value.dist){
         smallest = r;
     }
 
     if(smallest != i){
+        swap(A.priqueue[i].pos, A.priqueue[smallest].pos);
         swap(A.priqueue[i], A.priqueue[smallest]);
         minHeapify(A, smallest);
     }
 }
 
-MinHeap heapify(vector<infoNode>& A){ // O(n)
+MinHeap heapify(vector<HeapNode>& A){ // O(n)
     MinHeap H(A.size());
     H.priqueue = A;
 
@@ -70,12 +82,45 @@ MinHeap heapify(vector<infoNode>& A){ // O(n)
         minHeapify(H, i);
     }
 
-    for(infoNode &n : H.priqueue){ // O(n)
-        int index = n.node->index;
+    int posicion = 0; // Para asegurarnos que cada HeapNode no tenga posicion nula
+
+    for(HeapNode &n : H.priqueue){ // O(n)
+        int index = n.value.node->index;
         H.nodepair[index] = &n;
+        n.pos = posicion;
+        posicion++;
     }
 
     return H;
+}
+
+HeapNode min(MinHeap& A){ // O(1)
+    return A.priqueue[0];
+}
+
+HeapNode extractMin(MinHeap& A){ // O(log n)
+    if(A.size < 0){
+        throw out_of_range("La cola no tiene elementos");
+    }
+    HeapNode min = A.priqueue[0];
+    A.priqueue[0] = A.priqueue[A.size-1];
+    A.priqueue[0].pos = 0;
+    A.size -= 1;
+    minHeapify(A, 0);
+    return min;
+}
+
+void decreaseKey(MinHeap& A, int index, double key){
+    if(key > A.nodepair[index]->value.dist){
+        throw invalid_argument("Nueva key es mayor a la actual");
+    }
+    A.nodepair[index]->value.dist = key;
+    int pos = A.nodepair[index]->pos;
+    while(pos > 0 && A.priqueue[parent(pos)].value.dist > A.priqueue[pos].value.dist){
+        swap(A.priqueue[pos].pos, A.priqueue[parent(pos)].pos);
+        swap(A.priqueue[pos], A.priqueue[parent(pos)]);
+        pos = parent(pos);
+    }
 }
 
 int main() {
@@ -86,75 +131,104 @@ int main() {
     cout << setprecision(3);
     // Test minHeapify, mantener propiedad de minheap
 
-    vector<infoNode> A1;
-    vector<infoNode> A2;
+    vector<HeapNode> A1;
+    vector<HeapNode> A2;
 
     Graph* g = createGraph(7, 2, 4);
 
     //printGraph(*g);
 
-    cout << "\nTest minHeapify, mantener propiedad de minheap\n\n";
+    cout << "\nTest minHeapify, mantener propiedad de minheap:\n";
 
     for(int i = 0; i < 2; i++){
         //double r = urd(gen);
         double d = 0.99;
         infoNode inf1 = {d, (*g)[i]};
-        A1.push_back(inf1);
+        HeapNode hn1 = {inf1, i}; 
+        A1.push_back(hn1);
     }
 
     infoNode inf = {0.1, (*g)[g->size()-1]};
-    A1.push_back(inf);
+    HeapNode hn1 = {inf, 3}; 
+    A1.push_back(hn1);
 
     MinHeap H(A1.size());
 
     H.priqueue = A1;
-
-    for(infoNode n : H.priqueue){
-        cout << "(" << n.dist << ", " << n.node->index << ") ";
+    /*
+    for(HeapNode n : H.priqueue){
+        cout << "(" << n.value.dist << ", " << n.value.node->index << ") ";
     }
-    
+
     cout << "\n";
+    */
     minHeapify(H, 0);
 
-    for(infoNode n : H.priqueue){
-        cout << "(" << n.dist << ", " << n.node->index << ") ";
+    for(HeapNode n : H.priqueue){
+        cout << "(" << n.value.dist << ", " << n.value.node->index << ") ";
     }
 
 
-    cout << "\n\nTest heapify \n\n";
+    cout << "\n\nTest heapify: \n";
 
     for(int i = 0; i < g->size(); i++){
         double r = urd(gen);
         double d = 0.99;
         infoNode inf2 = {r, (*g)[i]};
-        A2.push_back(inf2);
+        HeapNode hn2 = {inf2, i};
+        A2.push_back(hn2);
     }
-
-    for(infoNode n : A2){
-        cout << "(" << n.dist << ", " << n.node->index << ") ";
+    /*
+    for(HeapNode n : A2){
+        cout << "(" << n.value.dist << ", " << n.value.node->index << ") posicion: " << n.pos << "\n";
     }
     
-    cout << "\n";
+    cout << "\n";*/
     MinHeap H2 = heapify(A2);
 
-
-    for(infoNode n : H2.priqueue){
-        cout << "(" << n.dist << ", " << n.node->index << ") ";
+    
+    for(HeapNode n : H2.priqueue){
+        cout << "(" << n.value.dist << ", " << n.value.node->index << ") posicion: " << n.pos << "\n";
     }
 
-    cout << "\n\nTest referencia de nodo a par que lo representa\n";
-
+    cout << "\n\nTest referencia de nodo a par que lo representa:\n";
+    /*
     for(Node* n : *g){
         int node_index = n->index;
         cout << "Indice de nodo: " << node_index;
-        infoNode* par = H2.nodepair[node_index];
-        cout << " Par que lo representa: " << "(" << par->dist << ", " << par->node->index << ") \n";
+        HeapNode* par = H2.nodepair[node_index];
+        cout << " Par que lo representa: " << "(" << par->value.dist << ", " << par->value.node->index << ") \n";
     }
     cout << "\n";
-    for(infoNode n : H2.priqueue){
-        int node_index = n.node->index;
+    for(HeapNode n : H2.priqueue){
+        int node_index = n.value.node->index;
         cout << "Indice de nodo: " << node_index;
-        infoNode* par = H2.nodepair[node_index];
-        cout << " Par que lo representa: " << "(" << par->dist << ", " << par->node->index << ") \n";
+        HeapNode* par = H2.nodepair[node_index];
+        cout << " Par que lo representa: " << "(" << par->value.dist << ", " << par->value.node->index << ") \n";
     }
+    */
+
+    cout << "\n\nTest min y extractMin:\n";
+
+    cout << "Par con dist. minima: " << "(" << min(H2).value.dist << ", " << min(H2).value.node->index << ") \n";
+
+    HeapNode min_pair = extractMin(H2);
+
+    cout << "Par con dist. minima extraido: " << "(" << min_pair.value.dist << ", " << min_pair.value.node->index << ") \n";
+
+    cout << "Heap luego de extraer minimo:\n";
+    for(int i = 0; i < H2.size; i++){
+        cout << "(" << H2.priqueue[i].value.dist << ", " << H2.priqueue[i].value.node->index << ") posicion: " << H2.priqueue[i].pos << "\n";
+    }
+
+    cout << "\nTest decreaseKey:\n";
+
+    decreaseKey(H2, 4, 0.01);
+
+    cout << "Heap luego de disminuir clave de nodo 4 a 0.01:\n";
+    for(int i = 0; i < H2.size; i++){
+        cout << "(" << H2.priqueue[i].value.dist << ", " << H2.priqueue[i].value.node->index << ") posicion: " << H2.priqueue[i].pos << "\n";
+    }
+
+
 }
